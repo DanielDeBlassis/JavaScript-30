96ac3540900ed8ef1edd9842b6b20864d558bce4
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-09-29 20:15:35 -0300

    Asigna ángulo a horas, minutos y segundos
    El cálculo almacenado en las variables es asignado a la propiedad transform de cada elemento, para aplicar una rotación gradual correspondiente a la hora, minutos, segundos.

--- Days/day-2/scripts/scripts.js
+++ Days/day-2/scripts/scripts.js
@@ -10,6 +10,11 @@ function establecerHora() {
     const segundos = tiempo.getSeconds();
 
     const anguloHoras = (((hora / 12) * 360) + 90 + (minutos / 2));//30° son una hora, por lo que se le suma la cantidad de minutos dividido por 2
+    $hora.style.transform = `rotate(${anguloHoras}deg)`;
+    
     const anguloMinutos = (((minutos / 60) * 360) + 90);//Se le suma 90 para corregir el ángulo inicial
+    $minutos.style.transform = `rotate(${anguloMinutos}deg)`;
+    
     const anguloSegundos = (((segundos / 60) * 360) + 90);//Se le suma 90 para corregir el ángulo inicial
+    $segundos.style.transform = `rotate(${anguloSegundos}deg)`;
 }
