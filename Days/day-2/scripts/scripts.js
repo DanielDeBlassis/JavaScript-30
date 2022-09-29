aa5b49ca7f6755c26695ebc87a1b314186bc07ba
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-09-29 20:14:00 -0300

    Calcula ángulo hora minutos y segundos
    En el caso de las horas, primero se divide por 12, se multiplica por 360 (grados del círculo), se le suma 90 (grados) correspondiente al ángulo inicial, y luego se suma la cantidad de minutos dividido 2 para que la aguja de horas avance mientras avanzan los minutos.
    Para los minutos y segundos simplemente se dividen por 60, luego se multiplican por 360 (grados en un círculo) y se suma 90 (ángulo inicial).

--- Days/day-2/scripts/scripts.js
+++ Days/day-2/scripts/scripts.js
@@ -9,4 +9,7 @@ function establecerHora() {
     const minutos = tiempo.getMinutes();
     const segundos = tiempo.getSeconds();
 
+    const anguloHoras = (((hora / 12) * 360) + 90 + (minutos / 2));//30° son una hora, por lo que se le suma la cantidad de minutos dividido por 2
+    const anguloMinutos = (((minutos / 60) * 360) + 90);//Se le suma 90 para corregir el ángulo inicial
+    const anguloSegundos = (((segundos / 60) * 360) + 90);//Se le suma 90 para corregir el ángulo inicial
 }
