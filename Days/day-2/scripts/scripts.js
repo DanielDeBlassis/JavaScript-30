bce2007f3b979a47eada71f74d10254c2430c9a3
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-09-29 20:09:33 -0300

    Crea función para establecer hora
    La misma crea una constante tiempo que toma la hora actual para luego tomar unidades específicas como la hora, los minutos y los segundos, guardándolo en variables.

--- Days/day-2/scripts/scripts.js
+++ Days/day-2/scripts/scripts.js
@@ -1,3 +1,12 @@
 const $hora = document.querySelector(".aguja.horas");
 const $minutos = document.querySelector(".aguja.minutos");
 const $segundos = document.querySelector(".aguja.segundos");
+
+function establecerHora() {
+    const tiempo = new Date();
+
+    const hora = tiempo.getHours();
+    const minutos = tiempo.getMinutes();
+    const segundos = tiempo.getSeconds();
+
+}
