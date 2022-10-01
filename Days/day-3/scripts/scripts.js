eaef3a3516bdb190b22aa56900d4b88339b09eed
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-01 00:00:40 -0300

    Crea función para actualizar valor variables
    Se crea una variable "suffix" la cual guardará el atributo "sizing" del elemento o una cadena vacía. Esto es para agregar el sufijo de medida al intentar setear un nuevo valor para la variable, ya que en CSS es necesario aclarar si son pixeles ("px") o cualquier otra medida, o en el caso del color al no ser necesario agregar ningún sufijo, se establecería como cadena vacía.
    Luego se setea la variable, tomando el nombre del input ("this.name") precedido por la nomenclatura de variables en CSS ("--"), y se establece el valor que será dado por el valor actual ("this.value") sumado al sufijo de medida ("suffix") si fuera necesario, y que sería de píxeles o una cadena vacía.

--- Days/day-3/scripts/scripts.js
+++ Days/day-3/scripts/scripts.js
@@ -1,2 +1,7 @@
 //Obtenemos todos los inputs de la página
 const $inputs = document.querySelectorAll("input");
+
+function actualizarValores(){
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
