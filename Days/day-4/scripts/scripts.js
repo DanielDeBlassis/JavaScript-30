6a49dfd0c06f22455cdb1e69b0eb0656309df1cb
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-03 08:11:12 -0300

    Crea array de inventores

---
+++ Days/day-4/scripts/scripts.js
@@ -0,0 +1,15 @@
+const inventors = [
+    { first: "Albert", last: "Einstein", year: 1879, passed: 1955, img: "http://drive.google.com/uc?export=view&id=1CAkx9phvc5MP4D7QmysZt606pmFA_w7Z" },
+    { first: "Isaac", last: "Newton", year: 1643, passed: 1727, img: "http://drive.google.com/uc?export=view&id=1k1c3QbZPYlp7aqdw0gWewQC1Snn2GHGx" },
+    { first: "Galileo", last: "Galilei", year: 1564, passed: 1642, img: "http://drive.google.com/uc?export=view&id=16GmHXIP5szQzLCE0V9AeHl8EoB9wYEn2" },
+    { first: "Marie", last: "Curie", year: 1867, passed: 1934, img: "http://drive.google.com/uc?export=view&id=1lyHJFITNYV3zEUyVSt_vjVnVBconJxHp" },
+    { first: "Johannes", last: "Kepler", year: 1571, passed: 1630, img: "http://drive.google.com/uc?export=view&id=1DyNvKFn_YfXR9wCQ1SbXy_WXC3L-EJM7" },
+    { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543, img: "http://drive.google.com/uc?export=view&id=1NseTZciL-D2WHYQwetR4hABwQA2N3imS" },
+    { first: "Max", last: "Planck", year: 1858, passed: 1947, img: "http://drive.google.com/uc?export=view&id=18K15olPNo9KlHkpJYwoD5WuE3PzqPGhV" },
+    { first: "Nikola", last: "Tesla", year: 1856, passed: 1943, img: "http://drive.google.com/uc?export=view&id=1lSQEDkl48vu6gqyvSw1Bh7pyoeeX2b0X" },
+    { first: "Thomas", last: "Edison", year: 1847, passed: 1931, img: "http://drive.google.com/uc?export=view&id=1qq6nWqfSyDlFTho0IOEbvCKiEENdYY_E" },
+    { first: "Benjamin", last: "Franklin", year: 1706, passed: 1790, img: "http://drive.google.com/uc?export=view&id=1DJl5y4D6bIyi9Od9BNR4NJKpjj3r2DbO" },
+    { first: "Leonardo", last: "Da Vinci", year: 1452, passed: 1519, img: "http://drive.google.com/uc?export=view&id=17w2VcDGfh4PaO12t8GWYxrtwEeyq41zg" },
+    { first: "Alexander", last: "Graham Bell", year: 1847, passed: 1922, img: "http://drive.google.com/uc?export=view&id=185xEfEBK5HUlNOC0RA_GZLwSQoPoQjJu" }
+];
+
