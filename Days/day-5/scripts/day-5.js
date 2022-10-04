16b55b7565412d4f22cce3173dcadd6a611cb5b8
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-03 21:13:17 -0300

    Agrega escucha de evento transición en paneles
    Al detectar una transición en cualquier panel o pestaña, llama a la función toggleActive.

--- Days/day-5/scripts/day-5.js
+++ Days/day-5/scripts/day-5.js
@@ -19,3 +19,5 @@ function toggleActive(e) {
 }
 
 $panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+$panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
+
